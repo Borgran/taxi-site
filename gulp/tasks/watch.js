0891d2f5948bfe982f248f97d3ece34e0af10c47
Piotr <piotr.boryszek@gmail.com>
2019-11-10 18:16:08 +0100

    Lesson 22

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('reloadCSS', function(){
+	return gulp.src('./app/buffor/styles/styles.css')
+	.pipe(browserSync.stream());
+});
+
+gulp.task('watch', function(){
+	
+	browserSync.init({
+		server: { 
+			baseDir: "app"
+		}
+	});
+	
+	watch('./app/index.html',function () {
+		console.log('HTML changed!');
+		browserSync.reload();
+	});	
+	
+	watch('./app/assets/styles/**/*.css', gulp.series("buildCSS", "reloadCSS", function () {
+		console.log('CSS updated and browser refresh!');					
+	}));	
+		
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,7 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
-cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-function defaultTask(task_name){
-	task_name();
-}
-exports.default = defaultTask;
 
-gulp.task('watch',function(){
-	watch('./app/index.html',function () {
-		console.log('HTML changed!');
-	});	
-	watch('./app/assets/styles/**/*.css',function () {
-		console.log('CSS changed!');
-		return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/buffor/styles'));
-		
-	});	
-});
+
+
 
