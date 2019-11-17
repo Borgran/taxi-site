1f1a78f147581a7e634491928ec6e1c51ae97e27
Piotr <piotr.boryszek@gmail.com>
2019-11-17 14:40:16 +0100

    Lesson 47 finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,10 @@
-import MobileMenu from './modules/MobileMenu.js';
+import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
-/*
-
-if (module.hot)
-{
+if (module.hot) {
 	module.hot.accept();
 }
-*/
 
 let MM = new MobileMenu();
+let RS = new RevealOnScroll();
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,53 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+class RevealOnScroll {
+	
+	constructor() {
+		this.itemToReveal = document.querySelectorAll(".feature-item");
+		this.browserHeight = window.innerHeight;
+		this.hideInit();
+		this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+		this.events();
+	}
+
+	hideInit(){
+		this.itemToReveal.forEach( e => { 
+			e.classList.add("reveal-item");
+			e.isRevealed = false;
+		});		
+		this.itemToReveal[this.itemToReveal.length - 1].isLastItem = true;
+	}
+	
+	events() {	
+		window.addEventListener("scroll", this.scrollThrottle);
+		window.addEventListener("resize", debounce(() => {
+			this.browserHeight = window.innerHeight;
+			console.log('Resize detected');
+		}, 333));
+	}
+	
+	calcCaller () {
+		this.itemToReveal.forEach( e => {
+			if (!e.isRevealed)
+			{
+			 	this.calculateIfScrolledTo(e);
+			}
+		});
+	}
+
+	calculateIfScrolledTo(e) {
+		if (window.scrollY + this.browserHeight > e.offsetTop) {/*Nie sprawdzaj jeśli dolna cześć okna jest wyżej niż element*/
+			let scrollPercent = (e.getBoundingClientRect().top / window.innerHeight) *100;
+			if (scrollPercent < 75) {/*Pokaż tylko gdy element sięgnie 75% okna*/
+				e.classList.add("reveal-item--is-visible");
+				e.isRevealed = true;
+				if (e.isLastItem == true) {
+					window.removeEventListener("scroll", this.scrollThrottle);
+				}
+			}		
+		}
+	}
+}
+
+export default RevealOnScroll;
